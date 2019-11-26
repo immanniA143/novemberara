d9bc1b742cb224d2b7aa94ddd937a24284a367a5
immanniA143 <aravindimmanmi333@gmail.com>
2019-11-26 18:09:57 +0530

    sample1

--- seleniumpractice/src/sample/Samo.java
+++ seleniumpractice/src/sample/Samo.java
@@ -3,8 +3,9 @@ package sample;
 public class Samo {
 
 	public static void main(String[] args) {
-		System.out.println("hgbfsgfjkshgfqtd");
+		System.out.println("aravind");
 		System.out.println("harish");
+		System.out.println("sr");
 	}
 
 }
