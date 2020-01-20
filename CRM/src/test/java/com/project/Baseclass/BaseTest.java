5f1c0141e3e0246f64aee1d3d94e1097c02d3835
immanniA143 <aravindimmanmi333@gmail.com>
2020-01-20 16:29:20 +0530

    Project_class1

---
+++ CRM/src/test/java/com/project/Baseclass/BaseTest.java
@@ -0,0 +1,39 @@
+package com.project.Baseclass;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseTest {
+	
+	public static FileInputStream fis;
+	public static Properties env,envProp;
+	public static String projectPath=System.getProperty("user.dir");
+	public static String envi;
+	
+
+	public static void init() {
+		try {
+			fis=new FileInputStream(projectPath+"//src//resource//java//Environment.properties");
+			env=new Properties();
+			env.load(fis);
+			envi = env.getProperty("env");
+			System.out.println(envi);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try 
+		{
+			fis=new FileInputStream(projectPath+"//src//resource//java//"+envi+".properties");
+			envProp=new Properties();
+			envProp.load(fis);
+			System.out.println(envProp.getProperty("fburl"));
+		} catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ CRM/src/test/java/com/project/Baseclass/Tc001.java
@@ -0,0 +1,12 @@
+package com.project.Baseclass;
+
+public class Tc001 extends BaseTest
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		init();
+	}
+
+}
